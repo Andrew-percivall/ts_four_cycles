4144803af1ffee4ff7e3efd1b578a0bdbfe953af
Andrew-percivall <Andry.sheremetew2@gmail.com>
2025-05-05 17:18:29 +0300

    5 done

--- index.ts
+++ index.ts
@@ -18,6 +18,9 @@
 //   console.log(`count : ${count}`)
 // }
 
+
+
+
 // # 2
 // let dollar: number = 41
 // console.log(`one dollar : ${dollar} UAH`);
@@ -26,6 +29,9 @@
 //   let dollars = dollar * x
 //   console.log(`${x} $ dollars = ${dollars} UAH`);
 // }
+
+
+
 
 // # 3
 // const num: number = Number(prompt('введіть квадрат чисел не більше 100: '))
@@ -49,31 +55,53 @@
 //   alert(`ви ввели число : ${num} а це число більше 100`);
 // }
 
+
+
+
 // # 4
-alert("З'ясувати, чи є число простим");
-let num: number;
-let trueNumber: boolean = true;
+// alert("З'ясувати, чи є число простим");
+// let num: number;
+// let trueNumber: boolean = true;
 
-do {
-  num = Number(prompt("введіть число :"));
+// do {
+//   num = Number(prompt("введіть число :"));
 
-  if (num === 1) alert("число 1 не є простим!");
-  else if (isNaN(num)) alert("ви ввели літери а потрібно ввести число");
-} while (isNaN(num) || num === 1);
+//   if (num === 1) alert("число 1 не є простим!");
+//   else if (isNaN(num)) alert("ви ввели літери а потрібно ввести число");
+// } while (isNaN(num) || num === 1);
 
-if (!num) alert(`ви натиснули кнопку відміни! або ввели пусте поле!`);
+// if (!num) alert(`ви натиснули кнопку відміни! або ввели пусте поле!`);
 
-if (num > 1) {
-  for (let i: number = 2; i < num; i++) {
-    if (num % i === 0) {
-      trueNumber = false;
-      break;
-    }
+// if (num > 1) {
+//   for (let i: number = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       trueNumber = false;
+//       break;
+//     }
+//   }
+
+//   if (trueNumber) {
+//     alert(`число: ${num} Просте число!`);
+//   } else {
+//     alert(`число: ${num} НЕ просте число`);
+//   }
+// }
+
+
+
+// # 5
+let num: number = Number(prompt('зведення числа у 3 степінь'));
+let sqrtNumber = false;
+
+for (let i:number = 0; i < num; i++) {
+
+  if (3 ** i === num) {
+    sqrtNumber = true
+    alert(`число ${num} = (3 ** ${i}) `)
+    break;
   }
+}
 
-  if (trueNumber) {
-    alert(`число: ${num} Просте число!`);
-  } else {
-    alert(`число: ${num} НЕ просте число`);
-  }
+if (!sqrtNumber) {
+  alert(` це число ${num} не являється у 3 степені`);
 }
